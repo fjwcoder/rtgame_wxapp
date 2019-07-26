04ddb1c5814db183ca7fe35b19be1cbf5a013a23
fjwcoder <fjwcoder@gmail.com>
2019-07-26 14:07:57 +0800

    wufuckshuo

--- siteinfo.js
+++ siteinfo.js
@@ -1,9 +1,8 @@
 module.exports = {
   name: "七剑游艺",
-
   siteroot: "https://qijian.fjwcoder.com/", // 必填: api地址，结尾要带/
   uploadroot: "https://qijian.fjwcoder.com/", // 文件上传地址
-  // siteroot: "http://192.168.0.17:8080/", // 必填: api地址，结尾要带/
-  // uploadroot: "http://192.168.0.17:8080/", // 文件上传地址
+  // siteroot: "http://rtgame.server/", // 必填: api地址，结尾要带/
+  // uploadroot: "http://rtgame.server/", // 文件上传地址
   uniacid: "10001", // 默认即可，勿填
 };
